ace4a7834c5efab5d5656b34e5519820b890efd8
22zagiva <49483236+22zagiva@users.noreply.github.com>
2019-05-01 09:02:36 -0400

    Add files via upload

---
+++ modb_unitconversion.py
@@ -0,0 +1,7 @@
+def main():
+    print("this program converts pounds to tons")
+    print()
+    p = float(input("please enter a number: "))
+    t = p * 0.0005
+    print(p,"pound(s) is",t,"ton(s)")
+main()
