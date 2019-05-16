4a03a59883d2a351a986afe741660b61cd4abf34
22zagiva <49483236+22zagiva@users.noreply.github.com>
2019-05-16 07:49:16 -0400

    Update modb_distance.py

--- modb_distance.py
+++ modb_distance.py
@@ -1,5 +1,5 @@
 def main():
     k = float(input("please enter the distance in kilometers: "))
-    m = round(k * 0.6214, 2)
+    m = round(k * 0.6214, 3)
     print(k,"kilometers is",m,"miles") 
 main()
